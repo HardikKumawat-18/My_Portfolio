1c0db8381dfcb45b886e63fe2d72911bf33b3ad6
HardikKumawat-18 <hardik.kumawat007@gmail.com>
2022-09-19 16:13:12 +0530

    Add Project details

---
+++ src/data/projectDetails.js
@@ -0,0 +1,99 @@
+export const docUp = {
+  projectLogoImg: "",
+  projectLogoURL: "",
+  projectTitle: "",
+  subTitle: "",
+  mockImg: "",
+  mockURL: "",
+  projectOverview: [
+    {
+      title: "Application Overview",
+      desc: "DocUp is a service platform & online community that allows people seek expert medical  It brings accountability & motivation to users from over 180 countries. I was one of two designers working on this project.",
+      roles: []
+    },
+    {
+      title: "My Roles",
+      desc: "",
+      roles: ["UI/UX Desiner", "Researcher"]
+    },
+    {
+      title: "Problems",
+      desc: "People spend a large amount of their time, energy and efforts for seeking medical guidance. Either they spend hours googling and ending up with deadly diseases on there search feed or they wait in endless queue for their medical appointments.",
+      roles: []
+    },
+    {
+      title: "Solution",
+      desc: "I designed an application (Currently for mobile platoforms) to allow users to seek medical guidance on their finger tips without wasting much of their time.",
+      roles: []
+    },
+  ],
+  designProcess: [
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+  ],
+  userResearch: "Lorem Ipsum",
+  problemIdentification: [
+    {
+      problemImg: "",
+      problemURL: "",
+      problemDesc: "",
+    },
+    {
+      problemImg: "",
+      problemURL: "",
+      problemDesc: "",
+    },
+    {
+      problemImg: "",
+      problemURL: "",
+      problemDesc: "",
+    },
+  ],
+  userJounrey: [
+    {
+      pictureImg: "",
+      pictureURL: "",
+    },
+  ],
+  wireframes: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  highFidelityPrototype: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  prototypeFeedback: [
+    {
+      userProfile: "",
+      userName: "",
+      feedback: "",
+    },
+  ],
+};
