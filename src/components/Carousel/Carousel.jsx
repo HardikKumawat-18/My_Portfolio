316264fb66ee3ca587c045d3616ce1522ef84a0a
Mr-Hypocrite <ym.jatin@gmail.com>
2022-10-10 01:21:16 +0530

    Carousel Responsive

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,6 +2,8 @@ import { Children, cloneElement, useEffect, useRef, useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import "./carousel.scss";
 
+const windowRes = window.innerWidth;
+
 export const CarouselItem = ({ children, width }) => {
   return (
     <div className="carousel-item" style={{ width: width }}>
@@ -63,13 +65,15 @@ export const Carousel = ({ children }) => {
           transform: `translateX(-${
             (activeIndex >= Children.count(children) - 2
               ? Children.count(children) - 3
-              : activeIndex) * 35
+              : activeIndex) * (windowRes <= 425 ? 100 : 35)
           }%)`,
         }}
         {...handlers}
       >
         {Children.map(children, (child, index) => {
-          return cloneElement(child, { width: "35%" });
+          return cloneElement(child, {
+            width: `${windowRes <= 425 ? 100 : 35}%`,
+          });
         })}
       </div>
 
